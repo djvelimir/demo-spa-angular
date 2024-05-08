8ce90ac69c7b5f6f89fc5d0c8d9b7ab4933962c1
Velimir Đurković <velimir.djurkovic@gmail.com>
2024-05-09 01:37:42 +0200

    Add Home, About and Contact page

---
+++ src/app/page-not-found/page-not-found.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PageNotFoundComponent } from './page-not-found.component';
+import { provideRouter } from '@angular/router';
+
+describe('PageNotFoundComponent', () => {
+  let component: PageNotFoundComponent;
+  let fixture: ComponentFixture<PageNotFoundComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PageNotFoundComponent],
+      providers: [provideRouter([])]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(PageNotFoundComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
